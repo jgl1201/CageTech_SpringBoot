891e1e8233903eef979955298f091d47bdc3c6f5
jgl1201 <jorgegarciacebem@gmail.com>
2024-05-26 22:45:12 +0200

    starting springboot project

---
+++ src/main/java/com/cagetech/cagetech/CagetechApplication.java
@@ -0,0 +1,13 @@
+package com.cagetech.cagetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CagetechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CagetechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cagetech/cagetech/CagetechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cagetech.cagetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CagetechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
