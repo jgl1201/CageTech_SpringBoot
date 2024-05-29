7b20597d667df04e6ab94dd509284661f9042cb0
unknown <jorgegarciacebem@gmail.com>
2024-05-29 11:21:40 +0200

    TODO DB and mvc

--- src/main/java/com/cagetech/cagetech/repositories/ArteMarcialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cagetech.cagetech.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.cagetech.cagetech.models.ArteMarcial;
-
-public interface ArteMarcialRepository extends CrudRepository<ArteMarcial, Long> {
-    
-}
--- src/main/java/com/cagetech/cagetech/repositories/EjercicioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cagetech.cagetech.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.cagetech.cagetech.models.Ejercicio;
-
-public interface EjercicioRepository extends CrudRepository<Ejercicio, Long>{
-    
-}
