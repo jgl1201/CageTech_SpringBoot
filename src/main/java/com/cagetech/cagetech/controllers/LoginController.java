a805df38e1e81bb92b76f31af1eb9f9e181dacbe
jgl1201 <jorgegarciacebem@gmail.com>
2024-06-12 11:18:42 +0200

    thymeleaf changes

--- src/main/java/com/cagetech/cagetech/controllers/LoginController.java
+++ src/main/java/com/cagetech/cagetech/controllers/LoginController.java
@@ -1,40 +1,15 @@
 package com.cagetech.cagetech.controllers;
 
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.cagetech.cagetech.utils.DBUtils;
 
+@Controller
 public class LoginController {
-
-    @Autowired
-    DBUtils dbUtils;
-
-    String userMail;
 
     @RequestMapping("/")
     public String login() {
         return "login";
     }
 
-    @PostMapping("/login")
-    public String doLogin(@RequestParam String email, @RequestParam String password, Map<String, Object> model) {
-        boolean validLogin = dbUtils.login(email, password);
-
-        if (validLogin) return "redirect:/rutinas"; 
-        else {
-            model.put("error", "Email o contraseña incorrectos");
-            return "login";
-        }
-    }
-
-    @RequestMapping("/register")
-    public String register() {
-        return "registration";
-    }
-    
 }
