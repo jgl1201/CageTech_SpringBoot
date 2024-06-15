6d809226bd0ca012ee0bb1a6c42777257722c560
jgl1201 <jorgegarciacebem@gmail.com>
2024-06-15 10:17:58 +0200

    user repository

--- src/main/java/com/cagetech/cagetech/models/UserModel.java
+++ src/main/java/com/cagetech/cagetech/models/UserModel.java
@@ -1,5 +1,57 @@
 package com.cagetech.cagetech.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Users")
 public class UserModel {
-    
+
+    @Id
+    private String email;
+    private String nombre;
+    private String apellidos;
+    private String contrasena;
+    private float peso;
+    private float altura;
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public String getContrasena() {
+        return contrasena;
+    }
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    public float getPeso() {
+        return peso;
+    }
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+    public float getAltura() {
+        return altura;
+    }
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
 }
