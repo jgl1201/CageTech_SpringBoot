4eb3282cb61b24412d86230830bd5f475a7deb85
jgl1201 <jorgegarciacebem@gmail.com>
2024-06-15 10:41:28 +0200

    db connection class

--- src/main/java/com/cagetech/cagetech/utils/DBConnection.java
+++ src/main/java/com/cagetech/cagetech/utils/DBConnection.java
@@ -4,24 +4,36 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import jakarta.annotation.PostConstruct;
+
 public class DBConnection {
 
     private Connection connector;
 
-    public DBConnection (String host, int port, String user, String password) {
-        this.connector = connect(host, port, user, password);
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String user;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @PostConstruct
+    public void init() {
+        this.connector = connect();
     }
 
-    public Connection connect(String host, int port, String user, String password) {
+    public Connection connect() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = "jdbc:mysql://" + host + ":" + port + "/";
             Connection con = DriverManager.getConnection(url, user, password);
             System.out.println("Conecion establecida");
             return con;
-        } catch (SQLException | ClassNotFoundException ex) {
-            ex.printStackTrace();
-            System.out.println("Error de conexion");
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
             return null;
         }
     }
