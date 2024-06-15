61c94f060e79c68b9960245dbaac9fd351fc929a
jgl1201 <jorgegarciacebem@gmail.com>
2024-06-15 16:30:44 +0200

    rutinas repoo

---
+++ src/main/java/com/cagetech/cagetech/repositories/RutinasRepository.java
@@ -0,0 +1,9 @@
+package com.cagetech.cagetech.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.cagetech.cagetech.models.RutinasModel;
+
+public interface RutinasRepository extends CrudRepository<RutinasModel, Integer> {
+    
+}
