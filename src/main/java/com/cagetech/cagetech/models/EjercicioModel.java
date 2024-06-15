3e02ae17dc3ae412e42deea943cf359051f7889a
jgl1201 <jorgegarciacebem@gmail.com>
2024-06-15 10:20:50 +0200

    ejercicios model

--- src/main/java/com/cagetech/cagetech/models/EjercicioModel.java
+++ src/main/java/com/cagetech/cagetech/models/EjercicioModel.java
@@ -1,5 +1,75 @@
 package com.cagetech.cagetech.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ejercicios")
 public class EjercicioModel {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private int series;
+    private int repeticiones;
+    private float peso;
+
+    @ManyToOne
+    @JoinColumn(name = "us_email")
+    private UserModel userModel;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public int getRepeticiones() {
+        return repeticiones;
+    }
+
+    public void setRepeticiones(int repeticiones) {
+        this.repeticiones = repeticiones;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public UserModel getUserModel() {
+        return userModel;
+    }
+
+    public void setUserModel(UserModel userModel) {
+        this.userModel = userModel;
+    }
+
 }
--- src/main/java/com/cagetech/cagetech/models/UserModel.java
+++ src/main/java/com/cagetech/cagetech/models/UserModel.java
@@ -1,6 +1,5 @@
 package com.cagetech.cagetech.models;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
